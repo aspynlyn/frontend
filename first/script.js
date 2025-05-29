9871e1978530bcd7b38e734a237a8de7ff9a19b2
aspynlyn <lyn.aspyn@gmail.com>
2025-05-29 13:20:47 +0900

    [docs]:프론트 복습

--- first/script.js
+++ first/script.js
@@ -16,5 +16,26 @@ if ($screen && $toggleButton) {
 
   $toggleButton.addEventListener('click', () => {
     stopWatchOn = !stopWatchOn;
+    console.log('stopWatchOn: ', stopWatchOn);
+    $toggleButton.innerHTML = stopWatchOn ? '■' : '▶';
+    $toggleButton.classList.toggle('stop-color');
+    // $toggleButton.style.backgroundColor = stopWatchOn ? 'tomato' : 'steelblue';
+    if (stopWatchOn) {
+      // 첫번째 인자: 콜백함수
+      // 두번째 인자: Number (ms)
+      timeInterval = setInterval(() => {
+        // 1초 > 100
+        // 1분 > 60초 > 6000
+        seconds++;
+        const mm = String(Math.floor(seconds / 6000) % 60).padStart(2, '0');
+        const ss = String(Math.floor(seconds / 100) % 60).padStart(2, '0');
+        const cs = String(seconds % 100).padStart(2, '0');
+        $screen.innerText = `${mm}:${ss}:${cs}`;
+      }, 10);
+    } else {
+      clearInterval(timeInterval);
+      seconds = 0;
+      $screen.innerText = '00:00:00';
+    }
   });
 }
